d94a4f703e87b37a20424e116ca742a21f1bcbdb
Prajapatipushkar <kppusakar2018@gmail.com>
2026-01-11 12:46:37 +0530

    added pattern_1.java

---
+++ src/patterns/pattern_1.java
@@ -0,0 +1,11 @@
+package src.patterns;
+//show the patterns name is Right half Pyramid
+public class pattern_1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
